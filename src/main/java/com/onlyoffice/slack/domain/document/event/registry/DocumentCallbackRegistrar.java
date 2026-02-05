6d8fb86afb2e0b7516d70e1c8f2955cdc163102f
Dmitrii Vershinin <dmitry.vershinin@onlyoffice.com>
2026-02-05 17:22:41 +0500

    docs: bumped copyrights

--- src/main/java/com/onlyoffice/slack/domain/document/event/registry/DocumentCallbackRegistrar.java
+++ src/main/java/com/onlyoffice/slack/domain/document/event/registry/DocumentCallbackRegistrar.java
@@ -1,5 +1,5 @@
 /**
- * (c) Copyright Ascensio System SIA 2025
+ * (c) Copyright Ascensio System SIA 2026
  *
  * <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
  * except in compliance with the License. You may obtain a copy of the License at
